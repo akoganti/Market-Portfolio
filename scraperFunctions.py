a2556501cb0cb9244adf1f44f5210a8094cd0754
Avinaash Koganti <avinaashkoganti@gmail.com>
2020-04-11 19:43:35 -0700

    cleaning up code

--- scraperFunctions.py
+++ scraperFunctions.py
@@ -5,6 +5,9 @@ import io
 import PyPDF2
 import pandas as pd
 import datetime
+import time
+from selenium import webdriver
+etfs = ['vti','vxus','bnd','bndx','vtip']
 
 def vtiScraper():
     vtiURL = "http://www.crsp.org/fact-sheet-archive"
@@ -156,3 +159,44 @@ def bndxScraper():
                     break
     bndxDF = pd.DataFrame({'date':pd.to_datetime(dates, infer_datetime_format=True),'Market Cap':marketCaps})
     return bndxDF
+
+def etfPriceData():
+    etf_price_data = pd.DataFrame(columns=['Date','ETF','Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
+    start_epoch = int(datetime.datetime(2000,1,1).timestamp())
+    curr_epoch = int(time.time())
+    for etf in etfs:
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/'+etf+'?period1='+str(start_epoch)+'&period2='+str(curr_epoch)+'&interval=1d&events=history'
+        try:
+            df = pd.read_csv(url,parse_dates=[0])
+        except ex.HTTPError:
+            # Including some breaks to avoid being blocked
+            time.sleep(20)
+            continue
+        df['ETF'] = etf.upper()
+        df['AdjClose'] = df['Adj Close'].astype('float64')
+        df = df.drop(['Adj Close'],axis=1)
+        df = df.dropna(subset=['AdjClose', 'Volume'])
+        df['Open']=df['Open'].astype('float64')
+        df['High']=df['High'].astype('float64')
+        df['Low']=df['Low'].astype('float64')
+        df['Close']=df['Close'].astype('float64')
+        df['Volume']=df['Volume'].astype('int64')
+        etf_price_data = etf_price_data.append(df)
+        etf_price_data = etf_price_data.reset_index(drop=True)
+    # Saving historical price data
+    etf_price_data.to_csv('etf_price_data.csv',index=False)
+
+def etfCurrPrices(etfList):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome('/Users/avinaashkoganti/chromedriver',options=options)
+    prices = []
+    for etf in etfList:
+        driver.get('https://finance.yahoo.com/quote/'+etf)
+        html = driver.page_source
+        soup = BeautifulSoup(html,features="html.parser")
+        x = soup.find('span', {'class':'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'})
+        price = float(x.text)
+        prices.append(price)
+    driver.close()
+    return prices
---
+++ updaterFunctions.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import io
+import PyPDF2
+import pandas as pd
+import datetime
+
+
+def vtiUpdater():
+    print('In development')
+
+def vxusUpdater():
+    print('In development')
+
+def bndUpdater():
+    print('In development')
+
+def bndxUpdater():
+    print('In development')
