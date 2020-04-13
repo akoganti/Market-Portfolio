087d79277df7b99cef2b855d7f48dd54e013b84c
Avinaash Koganti <avinaashkoganti@gmail.com>
2020-04-12 17:21:05 -0700

    Finished generating WBS for March 2020

---
+++ helperFunctions.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import datetime
+import time
+etfs = ['vti','vxus','bnd','bndx']
+
+def etfPriceData():
+    etf_price_data = pd.DataFrame(columns=['Date','ETF','Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
+    start_epoch = int(datetime.datetime(2000,1,1).timestamp())
+    curr_epoch = int(time.time())
+    for etf in etfs:
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/'+etf+'?period1='+str(start_epoch)+'&period2='+str(curr_epoch)+'&interval=1d&events=history'
+        try:
+            df = pd.read_csv(url,parse_dates=[0])
+        except ex.HTTPError:
+            # Including some breaks to avoid being blocked
+            time.sleep(20)
+            continue
+        df['ETF'] = etf.upper()
+        df['AdjClose'] = df['Adj Close'].astype('float64')
+        df = df.drop(['Adj Close'],axis=1)
+        df = df.dropna(subset=['AdjClose', 'Volume'])
+        df['Open']=df['Open'].astype('float64')
+        df['High']=df['High'].astype('float64')
+        df['Low']=df['Low'].astype('float64')
+        df['Close']=df['Close'].astype('float64')
+        df['Volume']=df['Volume'].astype('int64')
+        etf_price_data = etf_price_data.append(df)
+        etf_price_data = etf_price_data.reset_index(drop=True)
+    # Saving historical price data
+    etf_price_data.to_csv('etf_price_data.csv',index=False)
--- scraperFunctions.py
+++ scraperFunctions.py
@@ -6,8 +6,6 @@ import PyPDF2
 import pandas as pd
 import datetime
 import time
-from selenium import webdriver
-etfs = ['vti','vxus','bnd','bndx','vtip']
 
 def vtiScraper():
     vtiURL = "http://www.crsp.org/fact-sheet-archive"
@@ -25,16 +23,16 @@ def vtiScraper():
             pdf_page2 = read_pdf.getPage(1)
             page_text = pdf_page2.extractText()
             try:
-                market_cap = int(re.findall('INDEX MARKET CAP\d+,\d+,\d+', page_text, re.IGNORECASE)[0].upper().replace('INDEX MARKET CAP','').replace(',',''))
+                market_cap = int(re.findall('INDEX MARKET CAP\d+,\d+,\d+', page_text, re.IGNORECASE)[0].upper().replace('INDEX MARKET CAP','').replace(',',''))/1000
             except:
                 try:
                     page_text = pdf_page2.extractText().replace('\n','')
-                    market_cap = int(re.findall('INDEX MARKET CAP\d+,\d+,\d+', page_text, re.IGNORECASE)[0].upper().replace('INDEX MARKET CAP','').replace(',',''))
+                    market_cap = int(re.findall('INDEX MARKET CAP\d+,\d+,\d+', page_text, re.IGNORECASE)[0].upper().replace('INDEX MARKET CAP','').replace(',',''))/1000
                 except:
                     market_cap = 'n/a'
             marketCaps.append(market_cap)
     vtiDF = pd.DataFrame({'date':pd.to_datetime(dates, infer_datetime_format=True),'Market Cap':marketCaps})
-    return vtiDF
+    vtiDF.to_csv('vti_MC_data.csv',index=False)
 
 def vxusScraper():
     cur_year = datetime.datetime.now().year
@@ -71,14 +69,12 @@ def vxusScraper():
                         marketCaps.append(mc)
                         continue
                     else:
-                        mc_1 = mc[0].replace(',','')
-                        mc_2 = mc_1.replace('\n','')
-                        mc_3 = mc_2.replace('Totals','')
-                        mc = [int(s) for s in mc_3.split() if s.isdigit()][1]
+                        mc = mc[0].replace(',','').replace('\n','').replace('Totals','')
+                        mc = [int(s) for s in mc.split() if s.isdigit()][1] / 1000
                         marketCaps.append(mc)
                     break
     vxusDF = pd.DataFrame({'date':pd.to_datetime(dates, infer_datetime_format=True),'Market Cap':marketCaps})
-    return vxusDF
+    vxusDF.to_csv('vxus_MC_data.csv',index=False)
 
 def bndScraper():
     cur_year = datetime.datetime.now().year
@@ -118,7 +114,7 @@ def bndScraper():
                     marketCaps.append(mc)
                     break
     bndDF = pd.DataFrame({'date':pd.to_datetime(dates, infer_datetime_format=True),'Market Cap':marketCaps})
-    return bndDF
+    bndDF.to_csv('bnd_MC_data.csv',index=False)
 
 def bndxScraper():
     cur_year = datetime.datetime.now().year
@@ -158,45 +154,4 @@ def bndxScraper():
                     marketCaps.append(mc)
                     break
     bndxDF = pd.DataFrame({'date':pd.to_datetime(dates, infer_datetime_format=True),'Market Cap':marketCaps})
-    return bndxDF
-
-def etfPriceData():
-    etf_price_data = pd.DataFrame(columns=['Date','ETF','Open', 'High', 'Low', 'Close', 'AdjClose', 'Volume'])
-    start_epoch = int(datetime.datetime(2000,1,1).timestamp())
-    curr_epoch = int(time.time())
-    for etf in etfs:
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/'+etf+'?period1='+str(start_epoch)+'&period2='+str(curr_epoch)+'&interval=1d&events=history'
-        try:
-            df = pd.read_csv(url,parse_dates=[0])
-        except ex.HTTPError:
-            # Including some breaks to avoid being blocked
-            time.sleep(20)
-            continue
-        df['ETF'] = etf.upper()
-        df['AdjClose'] = df['Adj Close'].astype('float64')
-        df = df.drop(['Adj Close'],axis=1)
-        df = df.dropna(subset=['AdjClose', 'Volume'])
-        df['Open']=df['Open'].astype('float64')
-        df['High']=df['High'].astype('float64')
-        df['Low']=df['Low'].astype('float64')
-        df['Close']=df['Close'].astype('float64')
-        df['Volume']=df['Volume'].astype('int64')
-        etf_price_data = etf_price_data.append(df)
-        etf_price_data = etf_price_data.reset_index(drop=True)
-    # Saving historical price data
-    etf_price_data.to_csv('etf_price_data.csv',index=False)
-
-def etfCurrPrices(etfList):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome('/Users/avinaashkoganti/chromedriver',options=options)
-    prices = []
-    for etf in etfList:
-        driver.get('https://finance.yahoo.com/quote/'+etf)
-        html = driver.page_source
-        soup = BeautifulSoup(html,features="html.parser")
-        x = soup.find('span', {'class':'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'})
-        price = float(x.text)
-        prices.append(price)
-    driver.close()
-    return prices
+    bndxDF.to_csv('bndx_MC_data.csv',index=False)
